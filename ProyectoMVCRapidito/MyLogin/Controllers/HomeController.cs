b1028b1689f673fae0ba8534588f638bf534340c
Jul10CC <julio1cayax@outlook.es>
2022-05-20 21:29:19 -0600

    Primer Commit
    Julio Cayax

---
+++ ProyectoMVCRapidito/MyLogin/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyLogin.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(Usuarios u)
+        {
+            // this action is for handle post (login)
+            if (ModelState.IsValid) // this is check validity
+            {
+                using (DatabaseMyLoginEntities1 dc = new DatabaseMyLoginEntities1())
+                {
+                    var v = dc.Usuarios.Where(a => a.NombreUsuario.Equals(u.NombreUsuario) && a.Password.Equals(u.Password)).FirstOrDefault();
+                    if (v != null)
+                    {
+                        Session["LogedUserID"] = v.Id.ToString();
+                        Session["LogedUserFullname"] = v.NombreUsuario.ToString();
+                        return RedirectToAction("AfterLogin");
+                    }
+                }
+            }
+            return View(u);
+        }
+
+        public ActionResult AfterLogin()
+        {
+            if (Session["LogedUserID"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
